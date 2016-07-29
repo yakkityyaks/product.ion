367ba68a1413fbf9bb86251e38fde7fb3757525c
Andrew Leonardi <ndrewleonardi2@gmail.com>
2016-07-28 20:00:09 -0400

    (feat) A [server] accepts a new [organization] request
    Added a post route with mock data.

--- app/server/routes.js
+++ app/server/routes.js
@@ -1,4 +1,13 @@
 module.exports = function(app){
 
+  var data = {
+    username: 'yourUsernameValue',
+    password: 'yourPasswordValue',
+    organization: 'yourOrganziationName'
+  };
+
+  app.post('/register', function (req, res) {
+    res.status(201).send({data: data});
+  });
 
 };
