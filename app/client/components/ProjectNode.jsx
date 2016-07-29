18337fa4107e38caf9377b69ee9042c6abcfa342
Tim Clark <TC.Clark25@gmail.com>
2016-07-29 15:10:08 -0400

    Feat/Refactor: Updated app filetree. Added Main.jsx and ProjectNode.jsx. Added .jshintrc to gitignore

---
+++ app/client/components/ProjectNode.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router'
+// import CSSTransitionGroup from 'react-addons-css-transition-group';
+
+const ProjectNode = React.createClass({
+  render() {
+    // es6 syntax to create 3 variables that pull their data from
+    // this.props
+    // const { post, idx, comments } = this.props;
+    return (
+      <div className="projectNode">
+        I'm a project!
+      </div>
+    );
+
+  }
+
+});
+
+export default ProjectNode;
