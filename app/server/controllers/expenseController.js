95cb58485220ef460b21e6c75282366fcd8fde57
superhashbros <superhashbros42@gmail.com>
2016-07-29 10:42:10 -0400

    added dependencies, comments to controller files

--- app/server/controllers/expenseController.js
+++ app/server/controllers/expenseController.js
@@ -3,6 +3,15 @@
 //  exports.getUser = function(name, callback) {
 //    new User({username: name}).fetch().then(callback);
 //  };
+// 
+// this is example usage
+// 
+// app.post('/login', function(req, res) {
+//   Users.getUser(req.body.username, function(user) {
+//     user ? res.status(201).json(user) : res.sendStatus(404);
+//   });
+// });
+
 
 exports.getExpense(name, cb) {
 
--- app/server/controllers/organizationController.js
+++ app/server/controllers/organizationController.js
@@ -3,6 +3,14 @@
 //  exports.getUser = function(name, callback) {
 //    new User({username: name}).fetch().then(callback);
 //  };
+// 
+// this is example usage
+// 
+// app.post('/login', function(req, res) {
+//   Users.getUser(req.body.username, function(user) {
+//     user ? res.status(201).json(user) : res.sendStatus(404);
+//   });
+// });
 
 
 exports.getProjs(orgName, cb) {
