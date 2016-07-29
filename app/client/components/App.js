44ee6eeef84a17adfda1215cea1bb35962abd176
logosghost <jimkim777@gmail.com>
2016-07-29 19:07:11 -0400

    Let the great refactoring COMMENCE

--- app/client/components/App.js
+++ app/client/components/App.js
@@ -1,22 +1,19 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actionCreators from '../actions/actionCreators';
-import Main from './Main.js';
+import Main from './Main';
 
 function mapStateToProps(state) {
   return {
     posts: state.posts,
     comments: state.comments
-  };
+  }
 }
 
-//does a bit of magic, not entirely clear on what this does yet
-function mapDispatchToProps(dispatch) {
+function mapDispachToProps(dispatch) {
   return bindActionCreators(actionCreators, dispatch);
 }
 
-//adds all of the props and data from state to props
-// adds all of the action creators to props as well.
-const App = connect(mapStateToProps, mapDispatchToProps)(Main);
+const App = connect(mapStateToProps, mapDispachToProps)(Main);
 
 export default App;
---
+++ app/client/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// was comment.
+const Dashboard = React.createClass({
+
+  render() {
+    return (
+      <div>
+      Dashboard
+      </div>
+    )
+  }
+});
+
+export default Dashboard;
