1de42560d0d78a45ad66342dec079ab5a1092c11
Andrew Leonardi <ndrewleonardi2@gmail.com>
2016-07-30 11:39:24 -0400

    (feat) Server Routes
    Added preliminary route structure. Changed server to initialize webpack.

--- app/server/routes.js
+++ app/server/routes.js
@@ -1,23 +1,116 @@
-// var Expense = require('./controllers/expenseController.js');
-// var Organization = require('./controllers/organizationController.js');
-// var Project = require('./controllers/projectController.js');
-// var User = require('./controllers/userController.js');
+var Expense = require('./controllers/expenseController.js');
+var Organization = require('./controllers/organizationController.js');
+var Project = require('./controllers/projectController.js');
+var User = require('./controllers/userController.js');
 
-var path = require('path');
-
-console.log(__dirname);
 module.exports = function routes(app){
 
-  var data = {
-    username: 'yourUsernameValue',
-    password: 'yourPasswordValue',
-    organization: 'yourOrganziationName'
-  };
+  app.post('/register', function registerUser(req, res) {
+    var user = {
+      username: req.body.username,
+      password: req.body.password,
+      organization:req.body.orgs_Id
+    }
+    User.makeUser(user, function newUser(user){
+      // console.log('++++++++++line 15 user object', user)
+      user ? res.status(201).send(user) : res.sendStatus(404);
+    })
+  });
 
-  // app.post('/register', function registerUser(req, res) {
-  //   User.makeUser(req.body, function newUser(user){
-  //     user ? res.status(201).send({data: data}) : res.sendStatus(404);
-  //   })
-  // });
+  app.get('/login', function retrieveUser(){
+    var user = {
+      username: req.body.username,
+      password: req.body.password
+    }
+    User.getUser(user, function existingUser(user){
+      // console.log('++++++++++line 27 user object', user)
+      user ? res.status(200).send(user) : res.sendStatus(404);
+    })
+  });
+
+  app.post('/project', function createProject(req, res) {
+
+    var project = {
+      id: req.body.id,
+      orgs_id: req.body.orgs_id,
+      projId: req.body.projId,
+      name: req.body.name,
+      type: req.body.type,
+      reqBudget: req.body.reqBudget,
+      needs: req.body.needs,
+      shootDates: req.body.shootDates,
+      status: req.body.status,
+      costToDate: req.body.costToDate,
+      estimateToComplete: req.body.estimateToComplete
+    }
+
+    Project.makeProj(project, function newProject(project){
+      // console.log('++++++++++line 48 project object', project)
+      project ? res.status(201).send(project) : res.sendStatus(404);
+    })
+  });
+
+  app.get('/project', function retrieveProject(){
+    var project = {
+      id: req.body.id,
+      orgs_id: req.body.orgs_id,
+      projId: req.body.projId,
+      name: req.body.name,
+      type: req.body.type,
+      reqBudget: req.body.reqBudget,
+      needs: req.body.needs,
+      shootDates: req.body.shootDates,
+      status: req.body.status,
+      costToDate: req.body.costToDate,
+      estimateToComplete: req.body.estimateToComplete
+    }
+
+    project.getProj(project, function existingProject(project){
+      // console.log('++++++++++line 69 project object', project)
+      project ? res.status(200).send(project) : res.sendStatus(404);
+    })
+  });
+
+  app.post('/expense', function createExpense(req, res) {
+
+    var expense = {
+      id: req.body.id,
+      projsId: req.body.projsId,
+      type: req.body.type,
+      vertical: req.body.vertical,
+      glCode: req.body.glCode,
+      dateSpent: req.body.dateSpent,
+      dateTracked: req.body.dateTracked,
+      vendor: req.body.vendor,
+      method: req.body.method,
+      description: req.body.description,
+      cost: req.body.cost,
+    }
+
+    Expense.makeExpense(expense, function newExpense(expense){
+      // console.log('++++++++++line 91 expense object', expense)
+      expense ? res.status(201).send(expense) : res.sendStatus(404);
+    })
+  });
+
+  app.get('/expense', function(){
+    var expense = {
+      id: req.body.id,
+      projsId: req.body.projsId,
+      type: req.body.type,
+      vertical: req.body.vertical,
+      glCode: req.body.glCode,
+      dateSpent: req.body.dateSpent,
+      dateTracked: req.body.dateTracked,
+      vendor: req.body.vendor,
+      method: req.body.method,
+      description: req.body.description,
+      cost: req.body.cost,
+    }
+    Expense.getExpense(expense, function existingExpense(expense){
+      // console.log('++++++++++line 111 expense object', expense)
+      expense ? res.status(200).send(expense) : res.sendStatus(404);
+    })
+  });
 
 };
