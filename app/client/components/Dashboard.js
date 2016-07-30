2a8a819394bd740f76884068010214b2cb8cf829
Tim Clark <TC.Clark25@gmail.com>
2016-07-29 23:00:58 -0400

    Successfully implemented James refactor. State still needs definitions

--- app/client/components/Dashboard.js
+++ app/client/components/Dashboard.js
@@ -4,10 +4,12 @@ const Dashboard = React.createClass({
 
   render() {
     return (
-      <div>
-      Dashboard
+      <div className="dashboard">
+        I'm the dashboard!
+        {/* {this.props.projects.map((project, idx) =>
+          <Project key={idx} idx={idx} {...this.props} project={project}/>)} */}
       </div>
-    )
+    );
   }
 });
 
