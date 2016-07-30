3b722933523b29ab463cc7785b43751572038683
superhashbros <superhashbros42@gmail.com>
2016-07-30 11:39:51 -0400

    database now working

--- app/server/models/organization.js
+++ app/server/models/organization.js
@@ -1,17 +1,14 @@
 
 var Bookshelf = require('../db.js');
-var User  = require('./user.js');
-var Project = require('./project.js');
-
 
 var Organization = Bookshelf.Model.extend({
-	tableName: 'organizations',
+	tableName: 'orgs',
 	users: function() {
-		return this.hasMany(User);
+		return this.hasMany('User');
 	},
 	projects: function() {
-		return this.hasMany(Project);
+		return this.hasMany('Project');
 	}
 })
 
-module.exports = Organization;
+module.exports = Bookshelf.model('Organization', Organization);
