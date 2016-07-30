1698089fd4ed3ed47f0e642c514db36703d82dd4
logosghost <jimkim777@gmail.com>
2016-07-30 13:37:43 -0400

    Loginand create org Ready to hook up

---
+++ app/client/components/Organization.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { History } from 'react-router';
+// was comment.
+// const assign = Object.assign || require('object.assign');
+
+const Organization = React.createClass({
+
+  render() {
+    return (
+      <div>
+      <form className="form">
+         <div className="form__error-wrapper">
+           <p className="form__error form__error--username-taken">Sorry, but this username is already taken.</p>
+           <p className="form__error form__error--username-not-registered">This username does not exist.</p>
+           <p className="form__error form__error--wrong-password">Wrong password.</p>
+           <p className="form__error form__error--field-missing">Please fill out the entire form.</p>
+           <p className="form__error form__error--failed">Something went wrong, please try again!</p>
+         </div>
+         <div className="form__field-wrapper">
+           <input className="form__field-input" type="text" id="username" placeholder="Org Name" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
+           <label className="form__field-label" htmlFor="username">Organization</label>
+         </div>
+         <div className="form__field-wrapper">
+           <input className="form__field-input" type="text" id="username" placeholder="Admin Name" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
+           <label className="form__field-label" htmlFor="username">Admin</label>
+         </div>
+         <div className="form__field-wrapper">
+           <input className="form__field-input" id="password" type="password" placeholder="••••••••••" />
+           <label className="form__field-label" htmlFor="password">Password</label>
+         </div>
+         <div className="form__submit-btn-wrapper">
+             <button className="form__submit-btn" type="submit">SUBMIT</button>
+         </div>
+       </form>
+       </div>
+    );
+  }
+});
+
+export default Organization;
