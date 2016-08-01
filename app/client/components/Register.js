4ca553da3beac24f4d62f52ae389d5c4a79ef6ae
Tim Clark <TC.Clark25@gmail.com>
2016-08-01 16:40:49 -0400

    feat: front end now talks to back end

--- app/client/components/Register.js
+++ app/client/components/Register.js
@@ -9,11 +9,20 @@ const Register = React.createClass({
 
     }
   },
+  handleSubmit: function(event) {
+    event.preventDefault();
+    console.log(this.refs);
+    var orgName = this.refs.orgNameInput.value,
+        username = this.refs.usernameInput.value,
+        password = this.refs.passwordInput.value;
+
+    this.props.addNewOrg(orgName, username, password);
+  },
   render() {
     return (
       <div>
         <h2 className="form-page__form-heading">We're happy you want to do this</h2>
-        <form className="form" onSubmit={console.log("loginClicked")}>
+        <form className="form" onSubmit={this.handleSubmit}>
            <div className="form__error-wrapper">
              <p className="form__error form__error--username-taken">Sorry, but this username is already taken.</p>
              <p className="form__error form__error--invalid-password">Password has to be better than that.</p>
@@ -21,17 +30,22 @@ const Register = React.createClass({
              <p className="form__error form__error--failed">Something went wrong, please try again!</p>
            </div>
            <div className="form__field-wrapper">
-             <input className="form__field-input" type="text" id="username" placeholder="Org Name" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
              <label className="form__field-label" htmlFor="username">Organization</label>
+             <input className="form__field-input" type="text" id="username"
+                    placeholder="Org Name" autoCorrect="off" autoCapitalize="off"
+                    spellCheck="false" ref='orgNameInput' />
            </div>
            <div className="form__field-wrapper">
-             <input className="form__field-input" type="text" id="username" placeholder="Admin Name" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
              <label className="form__field-label" htmlFor="username">Admin</label>
+             <input className="form__field-input" type="text" id="username"
+                    placeholder="Admin Name" autoCorrect="off" autoCapitalize="off"
+                    spellCheck="false" ref='usernameInput'/>
            </div>
            <div className="form__field-wrapper">
              <label className="form__field-label" htmlFor="password">Password</label>
              <input className="form__field-input" id="password" type="password"
-                    placeholder="••••••••••" ref="passwordInput" onBlur="validatePassword"/>
+                    placeholder="••••••••••" ref="passwordInput"
+                    onBlur={this.validatePassword} />
            </div>
            <div className="form__submit-btn-wrapper">
                <button className="form__submit-btn" type="submit">SUBMIT</button>
---
+++ app/client/reducers/serverCalls.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+var register = function(organization, username, password) {
+
+  var data = {orgName: organization, username: username, password: password};
+  console.log("serverCalls: register: data is ", data);
+  return dispatch => {
+    return axios.post('/register', data);
+  };
+};
+
+var login = function(username, password) {
+  var data = {username: username, password: password};
+  return dispatch => {
+    return axios.post('/login', data);
+  };
+};
+
+
+const ApiCall = {
+  register,
+  login
+};
+
+export default ApiCall;
