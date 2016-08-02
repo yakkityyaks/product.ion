f2eed64bcf486b2a7cebc6af361021bb25f0982d
Andrew Leonardi <ndrewleonardi2@gmail.com>
2016-08-01 20:40:00 -0400

    (feat) Server Route Dashboard
    Added route for dashboard

--- app/server/routes.js
+++ app/server/routes.js
@@ -3,40 +3,86 @@ var Organization = require('./controllers/organizationController.js');
 var Project = require('./controllers/projectController.js');
 var User = require('./controllers/userController.js');
 
+//UNCOMMENT ANY CONSOLE LOG BELOW FOR TESTING PURPOSES
+
 module.exports = function routes(app){
 
-  app.post('/register', function registerUser(req, res) {
+  app.post('/api/register', function registerUser(req, res) {
     Organization.makeOrg({name: req.body.orgName}, function newOrg(org){
+      // console.log("++++The request body from axios is ", req.body);
       var user = {
         username: req.body.username,
         password: req.body.password,
         orgs_id: org.attributes.id
       };
-      console.log(req.body);
-      console.log(user);
       User.makeUser(user, function newUser(user){
+        // console.log("++++The request user object sent is ", user);
         user ? res.status(201).json(user) : res.sendStatus(404);
       });
     });
   });
 
-  app.post('/login', function retrieveUser(req, res){
+  app.post('/api/login', function retrieveUser(req, res){
+    // console.log("++++The request body from axios is ", req.body);
     User.getUser(req.body.username, function existingUser(user){
-      user ? res.status(200).json(user) : res.sendStatus(404);
+      // console.log("++++The request user object sent is ", user);
+      user ? res.status(201).json(user) : res.sendStatus(404);
     });
   });
 
-  app.post('/orgs', function(req, res) {
+  /* ORG OBJECT Line 68 returns:
+  {
+    "name": "ORGANIZATION NAME", //as string
+    "id": PRIMARY KEY, //assigned by db
+    "users": [
+      {
+        "id": PRIMARY KEY, //assigned by db
+        "username": "USER'S NAME", //as string
+        "password": "USER'S PASSWORD", //as string
+        "orgs_id": ORG ID USER BELONGS TO //as integer
+      },
+      //additional users in identical form
+    ],
+    "projects": [
+      {
+        "id": PRIMARY KEY, //assigned by db
+        "name": "PROJECT"S NAME, //as string
+        "projId": "PROJECT'S ORG LEVEL ID, //as string assigned by entry of org admin
+        "type": "ASSET TYPE", //as string
+        "reqBudget": "PROJECT'S TOTAL BUDGET", //as integer
+        "needs": "PROJECT NEEDS", //as string
+        "shootDates": "PROJECT SHOOTDATES", //as string
+        "status": "PROJECT STATUS", //as string
+        "costToDate": "PROJECT COST TO DATE", //as integer
+        "estimateToComplete": "PROJECT ESTIMATE TO COMPLETE ", //as string
+        "orgs_id": ORG ID PROJECT BELONGS TO //as integer
+      },
+      //additional projects in identical form
+    ]
+  }
+  */
+
+  app.post('/api/dashboard', function(req, res) {
+    // console.log("++++The request body from axios is ", req.body);
     Organization.getOrg(req.body.orgName, function(org) {
+      // console.log("++++The request org object sent is ", org);
       org ? res.status(201).json(org) : res.sendStatus(404);
     });
   });
 
-  //get proj by name
+  app.get('/api/org', function(req, res){
+    Organization.getOrg(req.body.orgName, function(org) {
+      // console.log("++++The request org object sent is ", org);
+      org ? res.status(200).json(org) : res.sendStatus(404);
+    });
+  });
 
-  //make proj w/ org name
-
-  //make expense w/ proj name
+  app.get('/api/project', function(req, res){
+    //hardcode req.body.name
+    Project.getProj('req.body.name', function existingProject(project){
+      project ? res.status(200).json(project) : res.sendStatus(404);
+    });
+  });
 
   // app.post('/project', function createProject(req, res) {
   //   Organization.getOrg(req.body.orgName, function getOrg(org){
