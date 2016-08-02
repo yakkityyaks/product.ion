e72dcd22f42fabd2fc65f1de903f07ac5d1b9ffb
Andrew Leonardi <ndrewleonardi2@gmail.com>
2016-08-02 19:50:24 -0400

    (feat) Pitch Component
    Added view routing to Pitch page to and from Dashboard route

--- app/client/components/Pitch.js
+++ app/client/components/Pitch.js
@@ -14,34 +14,38 @@ const Pitch = React.createClass({
            </div>
            <div className="form__field-wrapper">
              <label className="form__field-label" htmlFor="projectName">Project Name</label>
-             <input className="form__field-input" type="text" id="numberOfAssets" placeholder="Project Name" ref="projectName" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
+             <input className="form__field-input" type="text" id="numberOfAssets" placeholder="Please fill me out!" ref="projectName" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
            </div>
            <div className="form__field-wrapper">
              <label className="form__field-label" htmlFor="numberOfAssets">Number of Assets</label>
-             <input className="form__field-input" type="text" id="numberOfAssets" placeholder="Number of Assets" ref="numberOfAssetsInput" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
+             <input className="form__field-input" type="text" id="numberOfAssets" placeholder="Please fill me out!" ref="numberOfAssetsInput" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
            </div>
            <div className="form__field-wrapper">
              <label className="form__field-label" htmlFor="typeOfVideo">Type of Video</label>
-             <input className="form__field-input" type="text" id="typeOfVideo" placeholder="Video Type" ref="typeOfVideoInput" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
+             <input className="form__field-input" type="text" id="typeOfVideo" placeholder="Please fill me out" ref="typeOfVideoInput" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
            </div>
            <div className="form__field-wrapper">
              <label className="form__field-label" htmlFor="requestedBudget">Requested Budget</label>
-             <input className="form__field-input" type="text" id="requestedBudget" placeholder="Requested Budget" ref="requestedBudgetInput" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
+             <input className="form__field-input" type="text" id="requestedBudget" placeholder="Please fill me out" ref="requestedBudgetInput" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
            </div>
            <div className="form__field-wrapper">
              <label className="form__field-label" htmlFor="studioNeeds">Studio Needs</label>
-             <input className="form__field-input" type="text" id="studioNeeds" placeholder="Studio Needs" ref="studioNeedsInput" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
+             <input className="form__field-input" type="text" id="studioNeeds" placeholder="Please fill me out" ref="studioNeedsInput" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
            </div>
            <div className="form__field-wrapper">
              <label className="form__field-label" htmlFor="shootStartDate">Shoot Start Date</label>
-             <input className="form__field-input" type="text" id="shootStartDate" placeholder="Start Date" ref="shootStartDateInput" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
+             <input className="form__field-input" type="text" id="shootStartDate" placeholder="Please fill me out" ref="shootStartDateInput" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
            </div>
            <div className="form__field-wrapper">
              <label className="form__field-label" htmlFor="shootEndDate">Shoot End Date</label>
-             <input className="form__field-input" type="text" id="shootEndDate" placeholder="End Date" ref="shootEndDateInput" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
+             <input className="form__field-input" type="text" id="shootEndDate" placeholder="Please fill me out" ref="shootEndDateInput" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
            </div>
            <div className="form__submit-btn-wrapper">
-               <button className="form__submit-btn" type="submit">Submit Pitch!</button>
+             <button className="form__submit-btn" type="submit">
+               <Link to={`/dashboard/${this.props.organization.orgName.split(" ").join("")}`}>
+                 Submit your Pitch!
+               </Link>
+             </button>
            </div>
          </form>
       </div>
