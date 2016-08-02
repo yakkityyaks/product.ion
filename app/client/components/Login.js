465032afa04686920f8eb13290cf696d138890ce
logosghost <jimkim777@gmail.com>
2016-08-02 14:41:38 -0400

    styled and positioned buttons

--- app/client/components/Login.js
+++ app/client/components/Login.js
@@ -14,18 +14,21 @@ const Login = React.createClass({
     return (
       <div>
         <h2 className="form-page__form-heading">Login</h2>
-        <button>
-          <Link to={`/dashboard`}>
-            REGISTER TEST
-          </Link>
-        </button>
+
         <form className="form" onSubmit={this.handleSubmit}>
            {/* <div className="form__error-wrapper">
              <p className="form__error form__error--username-not-registered">This username does not exist.</p>
              <p className="form__error form__error--field-missing">Please fill out the entire form.</p>
              <p className="form__error form__error--failed">Something went wrong, please try again!</p>
            </div> */}
+           <button>
+             <Link to={`/dashboard`}>
+               REGISTER TEST
+             </Link>
+           </button>
+           <br></br>
            <div className="form__field-wrapper">
+
              <label className="form__field-label" htmlFor="username">Username</label>
              <br></br>
              <input className="form__field-input" type="text" id="username"
--- app/client/components/Main.js
+++ app/client/components/Main.js
@@ -9,10 +9,6 @@ const Main = React.createClass({
 
     return (
       <div>
-        <div>
-          <button><Link to="/register" className="btn btn--login btn--nav">Register</Link></button>
-          <button><Link to="/login" className="btn btn--login btn--nav">Login</Link></button>
-        </div>
         <div className="nav">
           <div className="nav__wrapper">
             <Link to="/" className="nav__logo-wrapper"><h1 className="nav__logo">product.ion</h1></Link>
