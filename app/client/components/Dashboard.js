60adc7b5c6571f6aadf3e763c89d703182b0ab37
Tim Clark <TC.Clark25@gmail.com>
2016-08-02 11:32:45 -0400

    Layed groundwork for server calls from Dashboard

--- app/client/components/Dashboard.js
+++ app/client/components/Dashboard.js
@@ -2,20 +2,10 @@ import React from 'react';
 import Projects from './Projects';
 import ProjectNode from './ProjectNode';
 // was comment.
-<<<<<<< afd8f6067a92a59fd0f3a1d32d5c18f8277528d3
-=======
 
 const Dashboard = React.createClass({
->>>>>>> foo
-
-const Dashboard = React.createClass({
-  // getInitialState() {
-  //   var orgName = this.props.organization.orgName;
-  //   console.log(this.props);
-  // },
   componentDidMount() {
-    var orgName = this.props.organization.orgName;
-    console.log(this.props.getProjectsByOrgName(orgName));
+    console.log(this);
   },
   render() {
     return (
