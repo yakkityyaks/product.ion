950794f4d099ee1864d88910325b814e62b226aa
Tim Clark <TC.Clark25@gmail.com>
2016-08-02 14:19:08 -0400

    feat: added more apicalls.

--- app/client/reducers/projects.js
+++ app/client/reducers/projects.js
@@ -1,7 +1,16 @@
+import ApiCall from "../utils/serverCalls";
+import store from "../store";
+
 function projects(state = [], action) {
   switch (action.type) {
-    case "NEW_PROJECT":
+    case "POST_NEW_PROJECT":
       console.log("You want to make a new project");
+      break;
+    case "GET_ORG_PROJECTS":
+      console.log("You want to get org projects");
+      ApiCall.getProjects(action.org)
+        .catch()
+        .then();
       break;
     case "HYDRATE_PROJECTS":
       console.log("You want to get some projects");
