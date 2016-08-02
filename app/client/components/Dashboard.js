95650ef8d105103470f77b01016daf19aa4b301c
superhashbros <superhashbros42@gmail.com>
2016-08-02 11:55:44 -0400

    foo

--- app/client/components/Dashboard.js
+++ app/client/components/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Projects from './Projects';
 import ProjectNode from './ProjectNode';
 // was comment.
+
 const Dashboard = React.createClass({
   componentDidMount() {
     console.log(this);
