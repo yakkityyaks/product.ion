c7a7c25cdaf3d51a0ecc4baf6db34dd0090abf9d
logosghost <jimkim777@gmail.com>
2016-08-03 16:05:37 -0400

    form validation, random password generator function

--- app/client/components/AddUser.js
+++ app/client/components/AddUser.js
@@ -18,11 +18,39 @@ const AddUser = React.createClass({
   // },
 
   // add on-change functions, dropdown for user class, logic that requires both forms filled out
+  validateUsername: function() {
+    var name = this.refs.usernameInput.value;
+    if (named.length < 6) {
+      console.log("Password must greater than 6 characters");
+    } else {
+      //trigger successful user view
+    }
+  },
+  randomPassword (length, username) {
+    let chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890";
+    let pass = "";
+    if(username.length >= 6) {
+
+      for (var x = 0; x < length; x++) {
+        var i = Math.floor(Math.random() * chars.length);
+        pass += chars.charAt(i);
+      }
+      return username + pass;
+    } else {
+      return "";
+    }
+  },
+
+  generate (event) {
+    event.preventDefault();
+    console.log(this.refs.passwordInput.value);
+    this.refs.passwordInput.value = this.randomPassword(3, this.refs.usernameInput.value);
+  },
+
   render() {
     return (
       <div>
-        <h2 className="form-page__form-heading">Create a New User</h2>
-
+        <h2 className="form-page__form-heading">Create a New Account</h2>
         <form className="form" onSubmit={this.handleSubmit}>
            {/* <div className="form__error-wrapper">
              <p className="form__error form__error--field-missing">Please fill out the entire form.</p>
@@ -38,29 +66,20 @@ const AddUser = React.createClass({
              <label className="form__field-label" htmlFor="username">Username</label>
              <br></br>
              <input className="form__field-input" type="text" id="username"
-                    placeholder="Username" ref="usernameInput"
-                    autoCorrect="off" autoCapitalize="off" spellCheck="false" />
+                    placeholder="Enter Username" ref="usernameInput"
+                    autoCorrect="off" autoCapitalize="off" spellCheck="false"
+                    minCharacters={6} validate={this.validateUsername} errorMessage="Name is invalid"
+                    emptyMessage="Name is required" onBlur={this.generate} />
            </div>
            <div className="form__field-wrapper">
              <label className="form__field-label" htmlFor="password">Password</label>
              <br></br>
-             <input className="form__field-input" id="password" type="password"
-                    ref="passwordInput" placeholder="••••••••••"/>
+             <input className="form__field-input" id="password" type="text"
+                ref="passwordInput" placeholder="Temporary Password Will Be Generated"/>
            </div>
-           <div className="form__field-wrapper">
-             <label className="form__field-label" htmlFor="password">Password</label>
-             <br></br>
-             <input className="form__field-input" id="password" type="password"
-                    ref="passwordInput" placeholder="••••••••••"/>
-           </div>
-           <div className="form__field-wrapper">
-             <label className="form__field-label" htmlFor="orgId">Organization ID</label>
-             <br></br>
-             <input className="form__field-input" id="org-id" type="text"
-                    ref="orgInput" placeholder="Organization ID"/>
-           </div>
+
            <div className="form__submit-btn-wrapper">
-               <button className="form__submit-btn" type="submit">Create New User</button>
+               <button className="form__submit-btn" type="submit">CREATE</button>
            </div>
          </form>
        </div>
