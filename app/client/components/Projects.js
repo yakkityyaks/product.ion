28be036a6c5d40aae98b1971bc0f27cba5c86dc8
superhashbros <superhashbros42@gmail.com>
2016-08-04 17:33:53 -0400

    working on bootstrap modals

--- app/client/components/Projects.js
+++ app/client/components/Projects.js
@@ -7,11 +7,29 @@ const Projects = React.createClass({
   render() {
     return (
       <div style={{fontSize : "14px"}}>
-        <button className="plus-button">
-          <Link to={`/pitch`}>
-            Add a Pitch!
-          </Link>
-        </button>
+        
+<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  Launch demo modal
+</button>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
       	<table style={{width: "100%"}}>
       		<thead>
       			<tr>
@@ -34,3 +52,10 @@ const Projects = React.createClass({
 });
 
 export default Projects;
+
+// <div style={{fontSize : "14px"}}>
+//         <button className="plus-button">
+//           <Link to={`/pitch`}>
+//             Add a Pitch!
+//           </Link>
+//         </button>
