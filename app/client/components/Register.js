4f6d7e826bddafcd296c33ea6bb25469676f7f58
Tim Clark <TC.Clark25@gmail.com>
2016-08-06 17:40:13 -0400

    Implemented bootstrap form validation for registration passwords

--- app/client/components/Register.js
+++ app/client/components/Register.js
@@ -1,24 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router';
-<<<<<<< beb9821ce17abf0ae678f8d2bd966c513e880fd5
-import { Button } from 'react-router';
-import { FormGroup } from 'react-router';
-import { FormControl } from 'react-router';
-import { ControlLabel } from 'react-router';
-// was comment.
-const Register = React.createClass({
-  getInitialState: function() {
-    return {
-      org: null,
-      usernameInput: null,            // fill me in tim
-      passwordInput: null
-    }
-  },
-  validatePassword: function() {
-    var pass = this.refs.passwordInput.value;
-    if (pass.length < 6) {
-      console.log("Password must greater than 6 characters");
-=======
 import { Button, Panel, FormGroup, FormControl, Form } from 'react-bootstrap';
 
 const Register = React.createClass({
@@ -37,7 +18,6 @@ const Register = React.createClass({
   handlePassChange(e) {
     if (this.validatePass() === "warning") {
       this.props.registrationError(4, "Password must be longer than 6 characters");
->>>>>>> Re-factored register to use bootstrap modals. Implemented partial validation
     } else {
       console.log("ding ding ding");
       this.props.resetRegistrationMessages();
@@ -54,48 +34,16 @@ const Register = React.createClass({
 
     this.props.checkRegistration(org, admin, pass);
   },
-  validationState: function() {
-    //fill me in tim
-  },
-  handleOrgChange: function() {
-    //fill me in tim
-  },
-  handleAdminChange: function() {
-    //fill me in tim
-  },
-  handlePassChange: function() {
-    //fill me in tim
-  },
-  submit: function() {
-
-  },
   render() {
     return (
-<<<<<<< beb9821ce17abf0ae678f8d2bd966c513e880fd5
-      <div>
-        <h2>We're happy you want to do this</h2>
-         <FormGroup validationState={this.validationState}>  
-=======
       <div id="loginPanel">
         <Panel bsStyle="primary" header={<h3>We're happy you want to do this</h3>}>
          <Form onSubmit={this.handleSubmit} onBlur={this.resetErrorMessages}>
->>>>>>> Re-factored register to use bootstrap modals. Implemented partial validation
            <Button>
              <Link to={`/login`}>
                Login
              </Link>
            </Button>
-<<<<<<< beb9821ce17abf0ae678f8d2bd966c513e880fd5
-           <ControlLabel>Organization</ControlLabel>
-           <FormControl type="text" value={this.state.org} placeholder="Org Name" onChange={this.handleOrgChange}/>  
-           <ControlLabel>Admin</ControlLabel>
-           <FormControl type="text" value={this.state.usernameInput} placeholder="Admin Name" onChange={this.handleAdminChange}/>  
-           <ControlLabel>Password></ControlLabel>
-           <FormControl type="password" value={this.state.passwordInput} placeholder="••••••••••" onChange={this.handlePassChange}/>
-           <Button onClick={this.submit}>Create</Button>
-         </FormGroup>  
-       </div>
-=======
            <br></br>
            <div className="">
              <label className="" htmlFor="username">Organization</label>
@@ -127,7 +75,6 @@ const Register = React.createClass({
          </Form>
       </Panel>
     </div>
->>>>>>> Re-factored register to use bootstrap modals. Implemented partial validation
     );
   }
 });
