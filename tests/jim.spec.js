af4ff39e9afee8cb653ce271da0f13a6d03a48f3
logosghost <jimkim777@gmail.com>
2016-08-12 10:09:29 -0400

    tests

--- tests/jim.spec.js
+++ tests/jim.spec.js
@@ -1,27 +1,22 @@
-//one test each using mocha.js
-
 import React from 'react';
 import expect from 'expect';
 import expectJSX from 'expect-jsx';
-import
-
-// spec to
 
 expect.extend(expectJSX);
 
+class TestComponent extends React.Component ({
 
 
-class TestComponent extends React.Component {}
+  describe('change password', () => {
+    it('should have new passwords that are the same', () => {
+      expect(newPassword).to.be(updatedPassword);
 
+    })
+    it('should update passwords that are not equal', () => {
+      expect(currentPassword).not.to.equal(inputPassword);
+    })
 
-
-describe('change password', () => {
-  it('should have new passwords that are the same', () => {
-    expect(newPassword).to.be(updatedPassword);
-  });
-  it('should update passwords that are not equal', () => {
-    expect(currentPassword).not.to.equal(inputPassword);
-  it('should not be empty', )
-    expect([1,2,3]).to.not.be.empty();
-  });
+    it('should not be empty', () => {
+      expect([1,2,3]).to.not.be.empty();
+  })
 });
