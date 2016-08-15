e83c23a27ddf500d4e43b354b1ca7b003d6bc298
Andrew Leonardi <ndrewleonardi2@gmail.com>
2016-08-15 14:55:48 -0400

    Merge Conflicts
    Adjusted for merge conflicts

--- app/client/components/Expenses.js
+++ app/client/components/Expenses.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router';
-<<<<<<< HEAD
 import ExpenseChart from './ExpenseChart';
 import { Button, ControlId, Form, FormControl, FormGroup, Modal, Panel, Table } from 'react-bootstrap';
 import ExpenseNode from './ExpenseNode';
 import NavBar from './NavBar';
 import CSVDrop from './CSVDrop';
-=======
-import { Button, ControlId, Form, FormControl, FormGroup, Modal, Panel, Table } from 'react-bootstrap';
-import ExpenseNode from './ExpenseNode';
-import NavBar from './NavBar';
->>>>>>> 5e1b99dc0de25f8d946f9866698764f9cb835087
-import FormTable from './formComponents/Table.js';
 
 const Expenses = React.createClass({
   getInitialState: function() {
     return {
-<<<<<<< HEAD
       open: false,
-=======
->>>>>>> 5e1b99dc0de25f8d946f9866698764f9cb835087
       expenses: this.props.expenses.expenses,
       projId: this.props.expenses.projId,
       count: 0,
@@ -34,20 +24,13 @@ const Expenses = React.createClass({
     var newCount = ++count;
     var addedExpenses = this.state.addedExpenses;
     addedExpenses.push(newCount);
-<<<<<<< HEAD
     this.setState({count : newCount});
-=======
-    this.setState({
-      count : newCount
-    });
->>>>>>> 5e1b99dc0de25f8d946f9866698764f9cb835087
   },
 
   removeExpenseNode(idx) {
     var last = this.state.addedExpenses.length-1
     var newExpensesCount = this.state.addedExpenses.slice(0,last);
     this.setState({addedExpenses : newExpensesCount});
-<<<<<<< HEAD
   },
 
   handleNewExpense: function(singleExpense){
@@ -58,16 +41,11 @@ const Expenses = React.createClass({
     this.props.postNewExpense(this.state.projId, singleExpense);
   },
 
-  handleSubmit: function(){
-    var newExpenses = this.state.newExpenses;
-
-  },
   switchModal () {
     this.props.changeModal('csv');
   },
   switchChart() {
     this.setState({open: !this.state.open});
-=======
   },
 
   handleNewExpense: function(singleExpense){
@@ -80,8 +58,6 @@ const Expenses = React.createClass({
 
   handleSubmit: function(){
     var newExpenses = this.state.newExpenses;
-
->>>>>>> 5e1b99dc0de25f8d946f9866698764f9cb835087
   },
 
   render() {
@@ -99,7 +75,6 @@ const Expenses = React.createClass({
         <Panel>
           <NavBar {...this.props}/>
         </Panel>
-<<<<<<< HEAD
         <h3>{"Expenses for " + projName }</h3>
         <Panel>
           <h3>Data Visualization!!!</h3>
@@ -108,9 +83,7 @@ const Expenses = React.createClass({
             {this.state.open ? <ExpenseChart {...this.props}/> : null}
             </Panel>
         </Panel>
-=======
         <h3>{"Project ID: "+ this.state.projId}</h3>
->>>>>>> 5e1b99dc0de25f8d946f9866698764f9cb835087
           <Table striped bordered condensed hover style={{width: "90%"}}>
               <thead>
                 <tr>
