8a74e40eb690228aea30b52ee45517f3f7e3ad75
logosghost <jimkim777@gmail.com>
2016-11-22 21:58:50 -0500

    (refactor)fix settings page

--- app/client/components/Settings.js
+++ app/client/components/Settings.js
@@ -4,9 +4,7 @@ import NavBar from './NavBar';
 import * as dom from 'react-dom';
 import UserList from "./UserList";
 import AddUser from './AddUser';
-import { Form, FormGroup, FormControl,
-  Grid, Row, Col, ControlLabel, Panel,
-  Button, Modal } from 'react-bootstrap';
+import { Form, FormGroup, FormControl, Grid, Row, Col, ControlLabel, Panel, Button, Modal } from 'react-bootstrap';
 
 const Settings = React.createClass({
   getInitialState() {
@@ -18,55 +16,36 @@ const Settings = React.createClass({
       validateMessage: ""
     };
   },
-  handleSubmit(e) {
+  handleSubmit (e) {
     e.preventDefault();
-    const { user } = this.props.organization;
-    if (user.perm === 0 && this.state
-      .newPass1 === this.state.newPass2
-    ) {
-      let currentPassword = this.state
-        .currentPass,
-        newPassword = this.state.newPass1;
-      this.props.changePassword(
-        user.name,
-        currentPassword,
-        newPassword);
+    const {user} = this.props.organization;
+    if (user.perm === 0 && this.state.newPass1 === this.state.newPass2) {
+      let currentPassword = this.state.currentPass,
+          newPassword = this.state.newPass1;
+      this.props.changePassword(user.name, currentPassword, newPassword);
     } else {
-      this.props.setPasswordMessage(
-        "You don't have permission to do that."
-      );
+      this.props.setPasswordMessage("You don't have permission to do that.");
     }
-    this.setState({ currentPass: "",
-      newPass1: "", newPass2: "" });
+    this.setState({currentPass: "", newPass1: "", newPass2: ""});
   },
-  changeOn(e) {
-    if (e.target.name ===
-      "currentPass") this.props.resetPasswordMessage();
-    if (e.target.name ===
-      "newPass2") {
-      if (e.target.value === this.state
-        .newPass1) {
-        this.setState({ validate: "success",
-          validateMessage: "" });
+  changeOn (e) {
+    if (e.target.name === "currentPass") this.props.resetPasswordMessage();
+    if (e.target.name === "newPass2") {
+      if (e.target.value === this.state.newPass1) {
+        this.setState({validate: "success", validateMessage: ""});
       } else {
-        this.setState({ validate: "error",
-          validateMessage: "Error! Passwords must match" });
+        this.setState({validate: "error", validateMessage: "Error! Passwords must match"});
       }
     }
-    this.setState({
-      [e.target.name]: e.target
-        .value });
+    this.setState({[e.target.name]: e.target.value});
   },
-  switchModal() {
-    this.props.changeModal(
-      'addUser');
+  switchModal () {
+    this.props.changeModal('addUser');
   },
 
   render() {
-    const { orgName, user } = this.props
-      .organization;
-    const permName = { 0: "an Admin",
-      1: "a Producer", 2: "a User" };
+    const { orgName, user } = this.props.organization;
+    const permName = { 0: "an Admin", 1: "a Producer", 2: "a User"};
     return (
       <div className="settings">
         <NavBar {...this.props}/>
@@ -86,48 +65,48 @@ const Settings = React.createClass({
              </Modal.Footer>
            </Modal>
            <div id="settingsWindow">
-            <div id="settingsMain">
-              <Form onSubmit={this.handleSubmit}>
-              <h3><strong>Change Password</strong></h3>
-                <FormGroup>
-                  <ControlLabel bsClass="chartSortSelector">Current Password</ControlLabel>&nbsp;
-                  <FormControl type="password" placeholder="••••••••••" required
-                              name="currentPass" autoCorrect="off" autoCapitalize="off"
-                              spellCheck="false" value={this.state.currentPass}
-                              onChange={this.changeOn}  />
-                  <p id="passwordMessage">{this.props.messages.password}</p>
-                </FormGroup>
-                <br></br>
-                <FormGroup validationState={this.state.validate}>
-                  <ControlLabel bsClass="chartSortSelector">New Password</ControlLabel>
-                  <FormControl type="password" placeholder="••••••••••" required
-                              autoCorrect="off" autoCapitalize="off"
-                              spellCheck="false" onChange={this.changeOn}
-                              name = "newPass1" value={this.state.newPass1}/>
-                  <FormControl.Feedback />
-                  <br></br>
-                  <ControlLabel bsClass="chartSortSelector" htmlFor="newPassword">Confirm New Password</ControlLabel>
-                  <FormControl type="password" placeholder="••••••••••"
-                              name="newPass2" autoCorrect="off"
-                              autoCapitalize="off" spellCheck="false"
-                              value={this.state.newPass2}
-                              onChange={this.changeOn} required />
-                  <FormControl.Feedback />
-                  <p id="passwordMessage">{this.state.validateMessage}</p>
-                </FormGroup>
-                <div>
-                  <Button type="submit" bsStyle="primary">
-                    Change Password
-                  </Button>
-                </div>
-              </Form>
-            </div>
-            <hr></hr>
-            <div id="settingsOptional">
-            {
-             !user.perm && <UserList {...this.props } switchModal={this.switchModal}/>
-            }
-            </div>
+                  <div id="settingsMain">
+                    <Form onSubmit={this.handleSubmit}>
+                    <h3><strong>Change Password</strong></h3>
+                      <FormGroup>
+                        <ControlLabel bsClass="chartSortSelector">Current Password</ControlLabel>&nbsp;
+                        <FormControl type="password" placeholder="••••••••••" required
+                                    name="currentPass" autoCorrect="off" autoCapitalize="off"
+                                    spellCheck="false" value={this.state.currentPass}
+                                    onChange={this.changeOn}  />
+                        <p id="passwordMessage">{this.props.messages.password}</p>
+                      </FormGroup>
+                      <br></br>
+                      <FormGroup validationState={this.state.validate}>
+                        <ControlLabel bsClass="chartSortSelector">New Password</ControlLabel>
+                        <FormControl type="password" placeholder="••••••••••" required
+                                    autoCorrect="off" autoCapitalize="off"
+                                    spellCheck="false" onChange={this.changeOn}
+                                    name = "newPass1" value={this.state.newPass1}/>
+                        <FormControl.Feedback />
+                        <br></br>
+                        <ControlLabel bsClass="chartSortSelector" htmlFor="newPassword">Confirm New Password</ControlLabel>
+                        <FormControl type="password" placeholder="••••••••••"
+                                    name="newPass2" autoCorrect="off"
+                                    autoCapitalize="off" spellCheck="false"
+                                    value={this.state.newPass2}
+                                    onChange={this.changeOn} required />
+                        <FormControl.Feedback />
+                        <p id="passwordMessage">{this.state.validateMessage}</p>
+                      </FormGroup>
+                      <div>
+                        <Button type="submit" bsStyle="primary">
+                          Change Password
+                        </Button>
+                      </div>
+                    </Form>
+                  </div>
+                  <hr></hr>
+                  <div id="settingsOptional">
+                  {
+                   !user.perm && <UserList {...this.props } switchModal={this.switchModal}/>
+                  }
+                  </div>
           </div>
         </Panel>
       </div>
